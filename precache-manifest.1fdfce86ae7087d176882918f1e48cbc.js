59af794a05c21b666facc00efaee6de82135e969
Nigar Abdullayeva <nigar_abdullaeva@hotmail.com>
2020-09-21 21:54:59 -0500

    Updates

---
+++ precache-manifest.1fdfce86ae7087d176882918f1e48cbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27289e25b84a2484bd3c6ce4f9a5d833",
+    "url": "/hw-employee-directory/index.html"
+  },
+  {
+    "revision": "33f4bd6f2e6441ae19c0",
+    "url": "/hw-employee-directory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "33f4bd6f2e6441ae19c0",
+    "url": "/hw-employee-directory/static/js/2.f89bf5f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hw-employee-directory/static/js/2.f89bf5f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e824b11c0d03985c26e0",
+    "url": "/hw-employee-directory/static/js/main.1c139ed4.chunk.js"
+  },
+  {
+    "revision": "13f7a3acb86efe8fe446",
+    "url": "/hw-employee-directory/static/js/runtime-main.569f8e66.js"
+  }
+]);
